5d85dbf1850db944022a97944344355cf267cea2
don undeen <donundeen@yahoo.com>
2024-11-10 11:59:37 -0500

    new code

--- html/index.js
+++ html/index.js
@@ -150,43 +150,6 @@ function correctedNow(){
     return  Date.now() + timeskew;
 }
 
-function processServerTime(msg){
-    console.log("processServerTime");
-//    let nowTimeStamp = Date.now();
-    let nowTimeStamp = correctedNow();
-    var clientTimestamp = msg.data.clientnow;    
-    let serverTimestamp = msg.data.servernow;
-    let responsediff = nowTimeStamp - msg.data.servernow;
-    let serverClientRequestDiffTime = msg.data.difference;
-    let serverClientResponseDiffTime = nowTimeStamp - serverTimestamp;
-
-    //https://stackoverflow.com/questions/1638337/the-best-way-to-synchronize-client-side-javascript-clock-with-server-date
-    var responseTime = (serverClientRequestDiffTime - nowTimeStamp + clientTimestamp - serverClientResponseDiffTime ) / 2
-    console.log("responseTime", responseTime);
-    // Calculate the synced server time
-    console.log("adjusted time ", nowTimeStamp + (serverClientResponseDiffTime - responseTime))
-    var syncedServerTime = new Date(nowTimeStamp + (serverClientResponseDiffTime - responseTime)).getTime();
-    
-    console.log("got server time",  syncedServerTime);
-
-    timeskew = syncedServerTime - nowTimeStamp;
-
-    timeobj = {
-        nowTimeStamp : nowTimeStamp,
-        clientTimestamp :clientTimestamp,   
-        serverTimestamp :serverTimestamp,
-        responsediff :responsediff,
-        serverClientRequestDiffTime :serverClientRequestDiffTime,
-        serverClientResponseDiffTime :serverClientResponseDiffTime,  
-        syncedServerTime : syncedServerTime,
-        skew: timeskew
-    }
-    
-    console.log(timeobj);    
-
-    $(".dbg").text(JSON.stringify(timeobj, null, "  "));
-}
-
 let notecount = 0;
 function midiMakeNote(pitch, velocity, duration){
     if(!started){
@@ -267,6 +230,11 @@ function load(data, name, starttime) {
                 playing = true;
                 player.play();        
             }, waittime);
+        }else{
+            let seektime = waittime * -1;
+            playing = true;
+            player.jumpMS(seektime);
+            player.play();
         }
     }
     catch (e) {
